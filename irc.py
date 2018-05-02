172d07e9dd61bcd371e48e58e7ea1162a2f0100b
daniel-Jones <daniel@danieljon.es>
2018-05-02 12:01:53 +0930

    send nothing instead of 0 (userlist)

--- irc.py
+++ irc.py
@@ -79,7 +79,7 @@ class Client:
 
         self.s.send(b':%s!salvo@127.0.0.1 JOIN %s\n' % (self.nick, channel_name))
         self.s.send(b':serenity 331 %s %s :%s\n' % (self.nick, channel_name, slchan.real_topic.encode('utf8')))
-        self.s.send(b':serenity 353 %s = %s :%s\n' % (self.nick, channel_name, b'0' if self.nouserlist else users))
+        self.s.send(b':serenity 353 %s = %s :%s\n' % (self.nick, channel_name, b'' if self.nouserlist else users))
         self.s.send(b':serenity 366 %s %s :End of NAMES list\n' % (self.nick, channel_name))
 
     def _privmsghandler(self, cmd: bytes) -> None:
