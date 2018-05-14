dc2f2ab7f9f4d67837f3e19b18e850de6ee43dc4
Raphaël Barrois <raphael.barrois@polytechnique.org>
2018-05-15 00:00:55 +0200

    Filter out old MPIMs in autojoin.
    The threshold for "too old chat" is set at 50 days, but could be
    user-defined.
    
    Note that, since we use the `rtm.connect` call (as advised in slack
    docs) and not `rtm.start`, the groups API will include MPIMs along
    with private chats; we need to filter them out specifically.
    
    Ref: https://api.slack.com/types/group "For compatibility with older
        clients, mpims can appear as private channels unless rtm.start is
        called with mpim_aware=1."

--- slack.py
+++ slack.py
@@ -16,6 +16,7 @@
 #
 # author Salvo "LtWorf" Tomaselli <tiposchi@tiscali.it>
 
+import datetime
 from functools import lru_cache
 from time import sleep
 from typing import *
@@ -63,6 +64,16 @@ class Topic(NamedTuple):
     value: str
 
 
+class LatestMessage(NamedTuple):
+    user: str  # The user id
+    text: str
+    ts: str  # The message UNIX timestamp, as text
+
+    @property
+    def timestamp(self):
+        return datetime.datetime.utcfromtimestamp(float(self.ts))
+
+
 class Channel(NamedTuple):
     """
     A channel description.
@@ -81,6 +92,8 @@ class Channel(NamedTuple):
     is_channel: bool = False
     is_group: bool = False
     is_mpim: bool = False
+
+    latest: Optional[LatestMessage] = None
 
     @property
     def name(self):
@@ -216,10 +229,24 @@ class Slack:
         else:
             raise ResponseException(response)
 
+        # Multiparty IM appear as both groups and mpim.
+        # Fetch MPIM first, to exclude them in the list of groups.
+        r = self.client.api_call("mpim.list", exclude_archived=True, exclude_members=True)
+        response = load(r, Response)
+        if response.ok:
+            mpims = load(r['groups'], List[Channel])
+            result.extend(mpims)
+            mpim_ids = [mpim.id for mpim in mpims]
+        else:
+            raise ResponseException(response)
+
         r = self.client.api_call("groups.list", exclude_archived=True, exclude_members=True)
         response = load(r, Response)
         if response.ok:
-            result.extend(load(r['groups'], List[Channel]))
+            result.extend([
+                group for group in load(r['groups'], List[Channel])
+                if group.id not in mpim_ids
+            ])
         else:
             raise ResponseException(response)
         return result
