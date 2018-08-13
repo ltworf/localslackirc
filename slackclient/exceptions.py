77f7275a3fb02052f0d0e4ecdfce2b9c88165d4e
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2018-08-13 22:50:19 +0200

    Remove unused exceptions

--- slackclient/exceptions.py
+++ slackclient/exceptions.py
@@ -32,20 +32,6 @@ class SlackClientError(Exception):
         super(SlackClientError, self).__init__(msg)
 
 
-class ParseResponseError(SlackClientError, ValueError):
-    """
-    Error raised when responses to Web API methods cannot be parsed as valid JSON
-    """
-    def __init__(self, response_body, original_exception) -> None:
-        super(ParseResponseError, self).__init__(
-            "Slack API response body could not be parsed: {0}. Original exception: {1}".format(
-                response_body, original_exception
-            )
-        )
-        self.response_body = response_body
-        self.original_exception = original_exception
-
-
 class SlackConnectionError(SlackClientError):
     def __init__(self, message='', reply=None) -> None:
         super(SlackConnectionError, self).__init__(message)
@@ -56,7 +42,3 @@ class SlackLoginError(SlackClientError):
     def __init__(self, message='', reply=None) -> None:
         super(SlackLoginError, self).__init__(message)
         self.reply = reply
-
-
-class SlackNotConnected(Exception):
-    pass
