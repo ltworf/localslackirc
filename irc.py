16f5da278de3bc14fae597f12ad8bbae8f228187
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2021-07-15 16:37:35 +0200

    Unindent block

--- irc.py
+++ irc.py
@@ -549,40 +549,39 @@ class Client:
                 i[mention.span()[1]:]
             )
 
-        if self.settings.provider == Provider.SLACK:
-            # Replace all channel mentions
-            while mention := _CHANNEL_MENTIONS_REGEXP.search(i):
-                i = (
-                    i[0:mention.span()[0]] +
-                    '#' +
-                    (await self.sl_client.get_channel(mention.groups()[0])).name_normalized +
-                    i[mention.span()[1]:]
-                )
+        # Replace all channel mentions
+        while mention := _CHANNEL_MENTIONS_REGEXP.search(i):
+            i = (
+                i[0:mention.span()[0]] +
+                '#' +
+                (await self.sl_client.get_channel(mention.groups()[0])).name_normalized +
+                i[mention.span()[1]:]
+            )
 
-            #Replace URL with bottom links
-            bottom = ""
-            refs = str.maketrans("0123456789", "⁰¹²³⁴⁵⁶⁷⁸⁹")
-            refn = 1
-            while url := _URL_REGEXP.search(i):
-                if not url:
-                    break
-                schema, path, label = url.groups()
-                if label and (label != f"{schema}://{path}"):
-                    ref = str(refn).translate(refs)
-                    refn += 1
-                    i = (
-                        i[0:url.span()[0]] +
-                        (f'{label}{ref}' if label else '') +
-                        i[url.span()[1]:]
-                    )
-                    bottom += f'\n  {ref} {schema}://{path}'
-                else:
-                    i = (
-                        i[0:url.span()[0]] +
-                        f'{schema}://{path}' +
-                        i[url.span()[1]:]
-                    )
-            i += bottom
+        #Replace URL with bottom links
+        bottom = ""
+        refs = str.maketrans("0123456789", "⁰¹²³⁴⁵⁶⁷⁸⁹")
+        refn = 1
+        while url := _URL_REGEXP.search(i):
+            if not url:
+                break
+            schema, path, label = url.groups()
+            if label and (label != f"{schema}://{path}"):
+                ref = str(refn).translate(refs)
+                refn += 1
+                i = (
+                    i[0:url.span()[0]] +
+                    (f'{label}{ref}' if label else '') +
+                    i[url.span()[1]:]
+                )
+                bottom += f'\n  {ref} {schema}://{path}'
+            else:
+                i = (
+                    i[0:url.span()[0]] +
+                    f'{schema}://{path}' +
+                    i[url.span()[1]:]
+                )
+        i += bottom
 
         for s in self.substitutions:
             i = i.replace(s[0], s[1])
