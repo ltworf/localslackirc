0e0d64d98665a60f4f1c6aed62013554225f8673
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2022-06-17 17:11:39 +0200

    Copyright notice

--- tests/test_irc.py
+++ tests/test_irc.py
@@ -1,5 +1,6 @@
 # localslackirc
 # Copyright (C) 2021 Antonio Terceiro
+# Copyright (C) 2022 Salvo "LtWorf" Tomaselli
 #
 # localslackirc is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
