fdd83464735a4a4fa512d2dab94ba93c0f9289f0
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2023-09-29 11:53:51 +0200

    Revert "Fix passing websocket connection timeout on RTM reconnects"
    This reverts commit 9a09fc2b2454138419e32dd150d67ad8a5508cec.

--- slackclient/client.py
+++ slackclient/client.py
@@ -123,7 +123,7 @@ class SlackClient:
         :Args:
             timeout: in seconds
         """
-        r = await self.login(timeout=timeout)
+        r = await self.login()
         headers = {
             'user-agent': 'localslackirc',
             'Authorization': f'Bearer {self._token}',
