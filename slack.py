1ae12ca3cdf3cd95d516f136275c530bac3d433c
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2023-11-14 20:27:39 +0100

    Bump year

--- slack.py
+++ slack.py
@@ -1,5 +1,5 @@
 # localslackirc
-# Copyright (C) 2018-2022 Salvo "LtWorf" Tomaselli
+# Copyright (C) 2018-2023 Salvo "LtWorf" Tomaselli
 #
 # localslackirc is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
